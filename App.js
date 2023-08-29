9253901ec101330bb5e2ac987c66a4285b685041
Kevin <kevin@tpe0925.acon.com>
2023-08-29 09:59:06 +0800

    Expense Tracker初始檔

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+    return <StatusBar style="auto" />;
+}
+
+const styles = StyleSheet.create({});
